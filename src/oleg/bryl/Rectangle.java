01b75f778ff4b0226fc20a609c68fbe7b54111e1
BADrobin <olegbryl@gmail.com>
2021-09-16 14:26:06 +0300

    almost 2

--- src/oleg/bryl/Rectangle.java
+++ src/oleg/bryl/Rectangle.java
@@ -1,5 +1,33 @@
 package oleg.bryl;
 
 public class Rectangle extends Form {
+    private Point inUpLeftPoint;
+    private Point bottomRightPoint;
 
+    public Rectangle(Point inUpLeftPoint, Point bottomRightPoint) {
+        this.inUpLeftPoint = inUpLeftPoint;
+        this.bottomRightPoint = bottomRightPoint;
+    }
+
+    public Rectangle(double left, double up, double right, double bottom) {
+        this.inUpLeftPoint = new Point(left, up);
+        this.bottomRightPoint = new Point(right, bottom);
+    }
+
+    @Override
+    public double square() {
+        return Math.abs((bottomRightPoint.getX() - inUpLeftPoint.getX()) *
+                (bottomRightPoint.getY() - inUpLeftPoint.getY()));
+    }
+
+    @Override
+    public double perimeter() {
+        return 0;
+    }
+
+    @Override
+    public void moving(double x, double y) {
+
+
+    }
 }
