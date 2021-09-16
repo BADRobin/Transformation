fc529fbb9dd3bfddc7a66e5ec97b80ff72c3a301
BADrobin <olegbryl@gmail.com>
2021-09-16 14:25:16 +0300

    almost

--- src/oleg/bryl/Circle.java
+++ src/oleg/bryl/Circle.java
@@ -1,6 +1,6 @@
 package oleg.bryl;
 
-public class Circle {
+public class Circle extends Form {
     private Point circleCenter;
     private double radius;
 
@@ -9,12 +9,54 @@ public class Circle {
     }
 
     public Circle(double x, double y, double radius) {
-
+        checkR(radius);
+        this.circleCenter = new Point(x, y);
     }
 
-    private void checkR(double radius) throws IllegalArgumentException{
-        if (radius <=0 ){
+    private void checkR(double radius) throws IllegalArgumentException {
+        if (radius <= 0) {
             throw new IllegalArgumentException("radius must be bigger then 000");
-        }this.radius = radius;
+        }
+        this.radius = radius;
+    }
+
+    @Override
+    public double square() {
+        return 3.14 * Math.sqrt(radius);
+    }
+
+    @Override
+    public double perimeter() {
+        return 2 * 3.14 * radius;
+    }
+
+    @Override
+    public void moving(double x, double y) {
+        circleCenter.setX(circleCenter.getX() + x);
+        circleCenter.setY(circleCenter.getY() + y);
+    }
+
+    public Point getCircleCenter() {
+        return circleCenter;
+    }
+
+    public void setCircleCenter(Point circleCenter) {
+        this.circleCenter = circleCenter;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "circleCenter=" + circleCenter +
+                ", radius=" + radius +
+                '}';
     }
 }
