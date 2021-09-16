dc14f54bf99dd51f0df904e034c719be69d2bb43
BADrobin <olegbryl@gmail.com>
2021-09-16 14:16:16 +0300

    one more step

---
+++ src/oleg/bryl/Circle.java
@@ -0,0 +1,15 @@
+package oleg.bryl;
+
+public class Circle {
+    private Point circleCenter;
+    private double radius;
+
+    public Circle(Point circleCenter, double radius) throws IllegalArgumentException {
+        this.circleCenter = circleCenter;
+    }
+
+    public Circle(double x, double y, double radius){
+
+    }
+
+}
